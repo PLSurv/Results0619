07031ebac047c6d07369da241d997ee15af97557
yi-apr <zhouyi0426@hotmail.com>
2020-07-30 20:10:03 +0900

    up

--- results/AppendixB4interval/AppB4.R
+++ results/AppendixB4interval/AppB4.R
@@ -25,25 +25,25 @@ times <- 1:max(DT$years)
 max.comp1 <- 5
 max.comp  <- 5
 LOOP4(file.name = "Comp3_G2_0.2.RData",I=I,max.comp = max.comp, times=times, ncut.var1 = 10, ncut.var2 = 60)
-p2 <- plot4.auc.pe("Comp3_G2_0.2.RData", times = times, data.name = "Gene2 with 17% censoring")
+p2 <- plot2.auc.pe("Comp3_G2_0.2.RData", times = times, data.name = "Gene2 with 17% censoring")
 
 #0.5cens
 #G2
 
 DT <- cbind.data.frame(ydata.5,gene2)
 times <- 1:max(DT$years)
-max.comp1 <- 5
-max.comp  <- 5
+max.comp1 <- 4
+max.comp  <- 4
 LOOP4(file.name = "Comp3_G2_0.5.RData",I=I,max.comp = max.comp, times=times, ncut.var1 = 10, ncut.var2 = 60)
-p4 <- plot4.auc.pe("Comp3_G2_0.5.RData", times = times, data.name = "Gene2 with 48% censoring")
+p4 <- plot2.auc.pe("Comp3_G2_0.5.RData", times = times, data.name = "Gene2 with 48% censoring")
 
 #0.7cens
 #G2
 
 DT <- cbind.data.frame(ydata.7,gene2)
 times <- 1:max(DT$years)
-max.comp1 <- 5
-max.comp  <- 5
+max.comp1 <- 4
+max.comp  <- 4
 LOOP4(file.name = "Comp3_G2_0.7.RData",I=I,max.comp = max.comp, times=times, ncut.var1 = 10, ncut.var2 = 60)
-p6 <- plot4.auc.pe("Comp3_G2_0.7.RData", times = times, data.name = "Gene2 with 67% censoring")
+p6 <- plot2.auc.pe("Comp3_G2_0.7.RData", times = times, data.name = "Gene2 with 67% censoring")
 
--- results/AppendixB4interval/AppB4.plot.save.R
+++ results/AppendixB4interval/AppB4.plot.save.R
@@ -7,21 +7,21 @@ source("../plot4.auc.pe.R")
 load("../../datasets/gene_test.RData")
 DT <- cbind.data.frame(ydata.2,gene2)
 times <- 1:max(DT$years)
-p2 <- plot4.auc.pe("Comp3_G2_0.2.RData", times = times, data.name = "Gene2 with 17% censoring")
+p2 <- plot2.auc.pe("Comp3_G2_0.2.RData", times = times, data.name = "Gene2 with 17% censoring")
 ggsave("PNG/Comp3_G2_0.2.png")
 #0.5cens
 #G2
 
 DT <- cbind.data.frame(ydata.5,gene2)
 times <- 1:max(DT$years)
-p4 <- plot4.auc.pe("Comp3_G2_0.5.RData", times = times, data.name = "Gene2 with 48% censoring")
+p4 <- plot2.auc.pe("Comp3_G2_0.5.RData", times = times, data.name = "Gene2 with 48% censoring")
 
 #0.7cens
 #G2
 
 DT <- cbind.data.frame(ydata.7,gene2)
 times <- 1:max(DT$years)
-p6 <- plot4.auc.pe("Comp3_G2_0.7.RData", times = times, data.name = "Gene2 with 67% censoring")
+p6 <- plot2.auc.pe("Comp3_G2_0.7.RData", times = times, data.name = "Gene2 with 67% censoring")
 
 P2 <- gridExtra::grid.arrange(p2, p4, p6, nrow = 3)
 #ggsave("~/Documents/Git_yiapr/Thesis/thesis/X_appendix/figB/cut.png", P2, width = 6, height = 9)
